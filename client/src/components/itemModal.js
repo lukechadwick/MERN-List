186ae5154cf9db76e71805ba74cccc1b3dd13ee2
luke <llukechadwick@gmail.com>
2019-02-09 22:34:14 +1300

    remove fake array

--- client/src/components/itemModal.js
+++ client/src/components/itemModal.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { addItem } from "../actions/ItemActions";
 import uuid from "uuid";
