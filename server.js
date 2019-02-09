9de2d4106141aa81ed62d13dfc0c86acd4161711
luke <llukechadwick@gmail.com>
2019-02-09 23:16:44 +1300

    set up for heroku deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const items = require("./routes/api/items");
 
@@ -18,8 +19,16 @@ mongoose
   .then(() => console.log("MongoDB connected..."))
   .catch(err => console.log(err));
 
-const port = process.env.port || 5000;
-app.listen(port, () => console.log(`Server started on port ${port}`));
-
 //Use Routes
 app.use("/api/items", items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.port || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
