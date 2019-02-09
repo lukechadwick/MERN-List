3c9596cc994afe1e94e4b255bb6149f881980fdb
luke <llukechadwick@gmail.com>
2019-02-09 22:57:57 +1300

    add delete item

--- client/src/actions/ItemActions.js
+++ client/src/actions/ItemActions.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 import { GET_ITEMS, DELETE_ITEM, ADD_ITEM, ITEMS_LOADING } from "./types";
+import { dispatch } from "rxjs/internal/observable/pairs";
 
 export const getItems = () => dispatch => {
   dispatch(setItemsLoading());
@@ -15,11 +16,13 @@ export const addItem = item => dispatch => {
     .then(res => dispatch({ type: ADD_ITEM, payload: res.data }));
 };
 
-export const deleteItem = id => {
-  return {
-    type: DELETE_ITEM,
-    payload: id
-  };
+export const deleteItem = id => dispatch => {
+  axios.delete(`/api/items/${id}`).then(res =>
+    dispatch({
+      type: DELETE_ITEM,
+      payload: id
+    })
+  );
 };
 
 export const setItemsLoading = () => {
